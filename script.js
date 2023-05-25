8a0ca680c7e6f904db282eccd92d6252e91217fe
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 22:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// JavaScript code
+window.addEventListener('DOMContentLoaded', function() {
+  var element = document.querySelector('#level');
+  var level = 0;
+
+  while (element.parentNode) {
+    element = element.parentNode;
+    level++;
+  }
+
+  alert("The level of the element is: " + level);
+});
+
